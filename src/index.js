89e20881d7539e170a3497da05951749482eef2a
Yashraj <yashrajdahiya0036@gmail.com>
2025-10-27 19:50:14 +0530

    added user and video model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB()
+connectDB().then(()=>{
+	app.listen(process.env.PORT || 8000,()=>{
+		console.log(`Server is running at port ${process.env.PORT}`)
+	})
+}).catch((e)=>{console.log("Mongo db connection failed.",e)})
 
 // (async ()=>{
 // 	try {
